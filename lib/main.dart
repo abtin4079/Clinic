29fd18871dd905cc7128c989829513f19b4225de
abtin4079 <abtin.aptitude@gmail.com>
2023-09-10 13:20:07 +0330

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -20,6 +20,10 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
 
+
+
+
+
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
